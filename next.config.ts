bc9d795ca51ea38c81f40419c6f561e3ddb267a5
Azaan Shehroz <azaanshehroz4@gmail.com>
2026-09-08 14:52:56 +0500

    pic allowed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["firebase-admin"],
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
