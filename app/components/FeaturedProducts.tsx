81609828c43ce7df0abcefe07eb522705841a55f
Azaan Shehroz <azaanshehroz4@gmail.com>
2026-09-26 16:29:03 +0500

    pinterset problem check

--- app/components/FeaturedProducts.tsx
+++ app/components/FeaturedProducts.tsx
@@ -1,19 +1,25 @@
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../lib/firebase";
 import FeaturedProductsClient from "./FeaturedProductsClient";
+import { adminDb } from "../lib/firebaseAdmin";
 
 export default async function FeaturedProducts() {
   try {
-    const snapshot = await getDocs(collection(db, "products"));
+    const snapshot = await adminDb.collection("products").get();
 
-    const products = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+   const products = snapshot.docs.map((doc) => {
+  const data = doc.data();
+
+  return {
+    id: doc.id,
+    ...data,
+    createdAt: data.createdAt?.toDate
+      ? data.createdAt.toDate().toISOString()
+      : data.createdAt ?? null,
+  };
+});
 
     return <FeaturedProductsClient initialProducts={products} />;
   } catch (error) {
-    console.error("Featured products error:", error);
+    console.error("Featured products server error:", error);
 
     return <FeaturedProductsClient initialProducts={[]} />;
   }
