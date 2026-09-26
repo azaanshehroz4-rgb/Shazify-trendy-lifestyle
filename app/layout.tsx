074075f5f541c54f5cae6fff5b5ac1a645ffb2a3
Azaan Shehroz <azaanshehroz4@gmail.com>
2026-09-26 09:54:19 +0500

    check file

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://shazify.shop"),
+  metadataBase: new URL("https://www.shazify.shop"),
 
   verification: {
     other: {
--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
 
-    sitemap: "https://shazify.shop/sitemap.xml",
+    sitemap: "https://www.shazify.shop/sitemap.xml",
   };
 }
